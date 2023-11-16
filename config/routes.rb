488f71d63537664296906b8c984aa74320884af2
menahil liaqat <zoyagee786@gmail.com>
2023-11-16 12:14:13 +1100

    added list destroy

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
 end
