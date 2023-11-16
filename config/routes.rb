964df922d1ab93706ede6b217e4d0c6ba371774f
menahil liaqat <zoyagee786@gmail.com>
2023-11-16 11:02:19 +1100

    style update

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
+  resources :bookmarks, only: [:destroy]
 end
