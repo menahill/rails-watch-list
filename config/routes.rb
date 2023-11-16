1e1cd4088a5808ee1e88880883d8245e091a2c68
menahil liaqat <zoyagee786@gmail.com>
2023-11-16 13:00:01 +1100

    change route home

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
